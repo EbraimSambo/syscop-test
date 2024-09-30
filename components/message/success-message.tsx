477830ffa280deb5fac682bb1c6f232ddff4de5e
pedromagdev <devpedromag@gmail.com>
2024-09-30 02:49:08 +0000

    success

--- components/message/success-message.tsx
+++ components/message/success-message.tsx
@@ -1,7 +1,7 @@
 const  SuccessMessage = ({message}:{message: string}) => {
     return (
         <div className='flex items-center justify-center'>
-            <p className="p-4 bg-successs-100 text-xs rounded-sm text-success-700"> {message} </p>
+            <p className="p-4 bg-success-100 text-xs rounded-sm text-success-700"> {message} </p>
         </div>
     );
 }
