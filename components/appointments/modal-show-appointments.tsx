7d66608b3b9349b000dcd0270abdce02daf74744
pedromagdev <devpedromag@gmail.com>
2024-09-30 06:52:18 +0000

    MOMMENT

--- components/appointments/modal-show-appointments.tsx
+++ components/appointments/modal-show-appointments.tsx
@@ -18,21 +18,16 @@ const ModalAppointments =() =>{
           <ModalContent>
             {(onClose) => (
               <>
-                <ModalHeader className="flex flex-col gap-1">FICHA DE AGENDAMENTO</ModalHeader>
+                <ModalHeader className="flex flex-col gap-1">
+                   <h2 className="text-center">FICHA DE AGENDAMENTO</h2>
+                </ModalHeader>
                 <ModalBody>
-                  <p> 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam pulvinar risus non risus hendrerit venenatis.
-                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                  </p>
+
                   <ShowAppointments uuid={uuid} />
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="light" onPress={closeModal}>
-                    Close
-                  </Button>
-                  <Button color="primary" onPress={closeModal}>
-                    Action
+                    Fechar Ficha
                   </Button>
                 </ModalFooter>
               </>
--- components/appointments/show-appointments.tsx
+++ components/appointments/show-appointments.tsx
@@ -1,15 +1,21 @@
 import { useFetcherClient } from '@/hooks/swr'
 import { Appointments } from '@/interfaces'
 import React from 'react'
+import ErrorLoading from '../errors/error-loader'
 
 function ShowAppointments({uuid}:{uuid: string}) {
     const {data, isLoading, error} = useFetcherClient<Appointments>(`appointments/${uuid}`)
     console.log(data)
     if(!uuid) return null
-    if(error) return <div>Erro..</div>
+    if(error) return <ErrorLoading />
     if(isLoading) return <div>carregando..</div>
   return (
-    <div>ShowAppointments</div>
+    <div>
+        <h2> <span className='font-bold'>Nome:</span>  {data?.name} </h2>
+        <h2> <span className='font-bold'>Telefone:</span>  {data?.phone} </h2>
+        <h2> <span className='font-bold'>Telefone:</span>  {data?.bi} </h2>
+        <h2> <span className='font-bold'>Dia da visita:</span>  {data?.dateVisit.toString()} </h2>
+    </div>
   )
 }
 
