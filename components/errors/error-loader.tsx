c032c7b3216a1d28f021711fbd224aa4b0dedc64
pedromagdev <devpedromag@gmail.com>
2024-09-30 00:34:37 +0000

    f

--- components/errors/error-loader.tsx
+++ components/errors/error-loader.tsx
@@ -1,14 +1,21 @@
 "use client"
 import { Button, Image } from "@nextui-org/react"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 
 
 const ErrorLoading = () =>{
     const [isLoading, setIsLoading] =    useState(false)
+    const router = useRouter()
     function reloadPage(){
         setIsLoading(true)
         return window.location.reload()
+    }
+
+    function resolutionProbleme(){
+        setIsLoading(true)
+        return router.refresh()
     }
     return(
         <div className="flex items-center justify-center p-4">
@@ -19,9 +26,13 @@ const ErrorLoading = () =>{
                     width={300}
                 />
                 <h1 className="text-3xl font-bold">Upps Alguma coisa falhou!</h1>
-                <p className="text-center font-bold text-xl">Estamos a tentar resolver o error, os dados voltaram em breve...</p>
-                <div className="">
-                    <Button color="primary" isLoading={isLoading} onClick={reloadPage}>Atualizar a pagina</Button>
+                <p className="text-center font-bold text-xl max-w-[400px] w-full mx-auto ">
+                    Estamos a tentar resolver o erro,
+                     os dados voltarão em breve...</p>
+                <div className="flex gap-4 items-center flex-wrap">
+                    <Button color="primary" isLoading={isLoading} onClick={reloadPage}>
+                        Atualizar a pagina
+                    </Button>
                 </div>
             </div>
         </div>
